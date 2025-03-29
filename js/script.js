2aa009166008e04c0303b7b6041780a69df0db21
atakanyuksell <atakanyukselis@hotmail.com>
2025-03-29 23:34:10 +0300

    inittial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Menü butonuna tıklanınca menüyü aç/kapat
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  document.querySelector(".navbar").classList.toggle("active");
+});
